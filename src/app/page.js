910de7cad91d5ffa3e14468492ef45ccda8f6829
Dmitri Khassanov <khdmitri@yahoo.com>
2023-06-14 10:15:06 +0400

    - Background - Banner component

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
-import Image from 'next/image'
 import styles from './page.module.css'
+import Banner from "@/components/banner";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>Coffee connoisseur</h1>
-    </main>
-  )
+    return (
+        <main className={styles.main}>
+            <h1 className={styles.title}>Coffee connoisseur</h1>
+            <Banner/>
+        </main>
+    )
 }
